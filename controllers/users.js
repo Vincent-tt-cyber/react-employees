90a153fef877d427e3de98e8c80288005aac0949
Evgeniy Petrov <evgeniy161102@gmail.com>
2023-11-02 18:51:28 +0300

    Доработана проверка авторизации

--- controllers/users.js
+++ controllers/users.js
@@ -9,27 +9,29 @@ const login = async (req, res) => {
     return res.status(400).json({
       message: "Пожалуйста, заполните обязательные поля",
     });
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
 
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-      },
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+
+  const secret = process.env.JWT_SECRET;
+
+  if (user && isPasswordCorrect && secret) {
+    return res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
     });
-
-    const isPasswordCorrect =
-      user && (await bcrypt.compare(password, user.password));
-
-    if (user && isPasswordCorrect) {
-      return res.status(200).json({
-        id: user.id,
-        email: user.email,
-        name: user.name,
-      });
-    } else {
-      return res.status(400).json({
-        message: "Неверно введен логин или пароль",
-      });
-    }
+  } else {
+    return res.status(400).json({
+      message: "Неверно введен логин или пароль",
+    });
   }
   res.send("login");
 };
