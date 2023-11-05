91684076b38d6c6a6cc09cb817afdf054289031a
Evgeniy Petrov <evgeniy161102@gmail.com>
2023-11-05 13:08:55 +0300

    Добавлена логика middleware (проверка токена)

--- controllers/users.js
+++ controllers/users.js
@@ -89,7 +89,7 @@ const register = async (req, res) => {
 };
 
 const current = async (req, res) => {
-  res.send("current");
+  return res.status(200).json(req.user);
 };
 module.exports = {
   login,
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { login, register, current } = require("../controllers/users");
+const { auth } = require("../middleware/auth");
 const router = express.Router();
 
 // /api/user/login
@@ -9,6 +10,6 @@ router.post("/login", login);
 router.post("/register", register);
 
 // /api/user/current
-router.get("/current", current);
+router.get("/current", auth, current);
 
 module.exports = router;
